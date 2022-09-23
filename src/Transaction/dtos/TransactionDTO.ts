5071dfe91f4a0e8f8d49d18ad4b90fbec721aea7
matheus <matheus.xmaz10@gmail.com>
2022-09-23 11:42:06 -0300

    Refatoração

--- src/Transaction/dtos/TransactionDTO.ts
+++ src/Transaction/dtos/TransactionDTO.ts
@@ -3,8 +3,18 @@ import { TranscationType } from "../enum/TransactionType";
 
 export interface TransactionDTO {
   id?: string;
-  userId?: string;
+  userId: string;
   value: number;
+  created_at: Date;
+  type: TranscationType;
+  user?: UserDTO;
+}
+
+export interface TransactionInsertDTO {
+  id?: string;
+  userId: string;
+  value: number;
+  created_at?: Date;
   type: TranscationType;
   user?: UserDTO;
 }
